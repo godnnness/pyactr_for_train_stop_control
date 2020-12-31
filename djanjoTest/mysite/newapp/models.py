0ac7523a397051877c566aed3bbbe6ffcfad0f24
godnnness <18120255@bjtu.edu.cn>
2020-12-31 10:20:45 +0800

    删除与算法无关的文件夹

--- djanjoTest/mysite/newapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Zhuce(models.Model):
-    user=models.CharField(max_length=20)
-    pwd=models.CharField(max_length=32)
-    ss=models.CharField(max_length=32)
-
